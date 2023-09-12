c19767ca6264dc5549a8b7be7068bd74fd3053dd
Сергей <pent2006@gmail.com>
2023-09-12 23:03:46 +0300

    add method __init__ add abstract method get_area add method add_area

--- src/Figure.py
+++ src/Figure.py
@@ -1,5 +1,17 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class Figure(ABC):
     """Declare figures methods"""
+
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def get_area(self):
+        pass
+
+    def add_area(self, other_figure):
+        if not isinstance(other_figure, Figure):
+            raise AssertionError("Can't add area")
+        return self.get_area() + other_figure.add_area
