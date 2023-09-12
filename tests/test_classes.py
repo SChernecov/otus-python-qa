9dd10f937bdfe8a35571050d0f553c665d4096a3
Сергей <pent2006@gmail.com>
2023-09-12 22:50:09 +0300

    add docstrings in test_classes.py

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,14 +1,18 @@
 class TestTriangle:
+    """Testing triangle"""
     pass
 
 
 class TestRectangle:
+    """Testing rectangle"""
     pass
 
 
 class TestSquare:
+    """Testing square"""
     pass
 
 
 class TestCircle:
+    """Testing circle"""
     pass
