60896fb7168c013e857c89fec433dcbc64fa3e98
Сергей <pent2006@gmail.com>
2023-09-13 02:00:22 +0300

    add method super().__init__() add property method get_area add methods get_area_rectangular_triangle and get_area_isosceles_triangle

--- src/Triangle.py
+++ src/Triangle.py
@@ -1,5 +1,27 @@
-from src import Figure
+from src.Figure import Figure, ABC
 
 
-class Triangle(Figure):
+class Triangle(Figure, ABC):
     """Declare triangle params"""
+
+    def __init__(self, side_a, side_b, side_c, shape, height):
+        super().__init__()
+        self.side_a = side_a
+        self.side_b = side_b
+        self.side_c = side_c
+        self.name = f"Triangle {side_a}, {side_b}, {side_c}"
+        self.shape = shape
+        self.height = height
+
+    @property
+    def get_area(self):
+        if self.shape == "rectangle_triangle":
+            return self.get_area_rectangular_triangle()
+        if self.shape == "isosceles_triangle":
+            return self.get_area_isosceles_triangle(self.height)
+
+    def get_area_rectangular_triangle(self):
+        return (self.side_a * self.side_b) / 2
+
+    def get_area_isosceles_triangle(self, height):
+        return 1 / 2 * self.side_b * height
