91246643ea4cc7b25f36a9c31d6b851b86880485
Сергей <pent2006@gmail.com>
2023-09-13 13:44:58 +0300

    add methods in random_helper.py

--- helpers/random_helper.py
+++ helpers/random_helper.py
@@ -1,16 +1,23 @@
 import random
 
+ru_lowercase = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+ru_uppercase = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+en_lowercase = 'abcdefghijklmnopqrstuvwxyz'
+en_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+punctuations = r"""!"#$%&'()*+,-—–./:;<=>?@[\]^_`{|}~№«»“”„‹›‘’‚"""
+whitespaces = ' \t\n\r\v\f'
+
 
 def random_small_side():
-    """Generate random side between 10 and 100"""
-
-    return random.randint(10, 100)
-
-
-def random_big_side():
     """Generate random side between 1 and 10"""
 
     return random.randint(1, 10)
+
+
+def random_big_side():
+    """Generate random side between 10 and 100"""
+
+    return random.randint(10, 100)
 
 
 def random_triangle_side():
@@ -19,7 +26,26 @@ def random_triangle_side():
     return random.choice([random_small_side(), random_big_side()])
 
 
-def random_height():
-    """Generate random height between 1 and 10"""
+def random_lower_letters():
+    """Generate random ru and en letters"""
 
-    return random.randint(1, 100)
+    letters = ru_lowercase + en_lowercase + ru_uppercase + en_uppercase
+    return ''.join(random.choice(letters) for _ in range(10))
+
+
+def random_punctuations():
+    """Generate random punctuations"""
+
+    return ''.join(random.choice(punctuations) for _ in range(10))
+
+
+def random_whitespaces():
+    """Generate random whitespaces"""
+
+    return ''.join(random.choice(whitespaces) for _ in range(10))
+
+
+def random_bool():
+    """Generate random bool param"""
+
+    return random.choice([True, False])
