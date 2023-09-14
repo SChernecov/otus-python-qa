7b9532be5d4bbdc785e98aef8c1bc6c1be1ce5ad
Сергей <pent2006@gmail.com>
2023-09-15 01:10:38 +0300

    add logic in Circle.py

--- src/Circle.py
+++ src/Circle.py
@@ -1,5 +1,24 @@
-from src import Figure
+import math
+
+from src.Figure import Figure
 
 
 class Circle(Figure):
     """Declare circle params"""
+
+    def __init__(self, radius):
+        super().__init__()
+        if type(radius) != float and type(radius) != int:
+            raise ValueError("Radius must be float or int")
+        if radius <= 0:
+            raise ValueError("Radius must be greater, then zero")
+        self.radius = radius
+        self.name = f"Circle with radius {radius}"
+
+    @property
+    def get_area(self):
+        return math.pi * (self.radius ** 2)
+
+    @property
+    def get_perimeter(self):
+        return 2 * math.pi * self.radius
