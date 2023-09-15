210a8e3876c9c7d896f199f9a3ac2b3fa615eda3
Сергей <pent2006@gmail.com>
2023-09-16 00:32:59 +0300

    change logic in Triangle.py

--- src/Triangle.py
+++ src/Triangle.py
@@ -12,6 +12,11 @@ class Triangle(Figure):
             raise ValueError("Sides must be integers")
         if side_a <= 0 or side_b <= 0 or side_c <= 0:
             raise ValueError("Sides must be greater, then zero")
+        if side_a + side_b > side_c and side_a + side_c > side_b \
+                and side_b + side_c > side_a:
+            raise ValueError(
+                "The length of side must be less, than the sum of the lengths"
+                " two other sides")
         self.side_a = side_a
         self.side_b = side_b
         self.side_c = side_c
