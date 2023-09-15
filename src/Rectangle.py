4c73eed0acdc065011a92894f3165e0ee3fdb0d8
Сергей <pent2006@gmail.com>
2023-09-15 20:17:15 +0300

    remove two sides and some conditions from super class

--- src/Rectangle.py
+++ src/Rectangle.py
@@ -4,22 +4,15 @@ from src.Figure import Figure
 class Rectangle(Figure):
     """Declare rectangle params"""
 
-    def __init__(self, side_a, side_b, side_c, side_d):
+    def __init__(self, side_a, side_b):
         super().__init__()
-        if type(side_a) != int or type(side_b) != int or type(
-                side_c) != int or type(side_d) != int:
+        if type(side_a) != int or type(side_b) != int:
             raise ValueError("Sides must be integers")
-        if side_a != side_c or side_b != side_d:
-            raise ValueError("This is not a rectangle")
-        if side_a == side_b or side_c == side_d:
-            raise ValueError("This is square, not rectangle")
-        if side_a <= 0 or side_b <= 0 or side_c <= 0 or side_d <= 0:
+        if side_a <= 0 or side_b <= 0:
             raise ValueError("Sides must be greater, then zero")
         self.side_a = side_a
         self.side_b = side_b
-        self.side_c = side_c
-        self.side_d = side_d
-        self.name = f"Rectangle {side_a}, {side_b}, {side_c}, {side_d}"
+        self.name = f"Rectangle {side_a}, {side_b}"
 
     @property
     def get_area(self):
